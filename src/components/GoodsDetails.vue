50cc634f3a3c3287d2e7bc19546e2793217dc6fd
shaonhuang <yukuaihuang@icloud.com>
2020-11-24 18:26:29 +0800

    second upload

--- src/components/GoodsDetails.vue
+++ src/components/GoodsDetails.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <section>
+    <section v-if="this.shoppingPageStep === 0">
       <div id="bottomPageGoodsDetailsContainer">
         <div id="bottomPageSlideshow">
           <el-carousel :interval="4000" type="card" height="400px">
-            <el-carousel-item v-for="item in 6" :key="item">
+            <el-carousel-item v-for="item in 3" :key="item">
               <h3 class="medium">{{ item }}</h3>
             </el-carousel-item>
           </el-carousel>
@@ -15,44 +15,50 @@
         <div id="bottomPageImageAndContents">
           <div id="bottomPageFirstImageAndContent">
             <img src="" alt="nothing to show" />
-            <span>产品介绍</span>
+            <div><span>产品介绍</span></div>
           </div>
           <div id="bottomPageSecondImageAndContent">
-            <span>产品介绍</span>
+            <div><span>产品介绍</span></div>
             <img src="" alt="nothing to show" />
           </div>
         </div>
-        <div><button>现在订购</button></div>
-      </div>
-    </section>
-    <section v-if="false">
-      <div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <div>
-            <span></span>
-            <input type="text" />
-          </div>
-          <div>
-            <span></span>
-            <input type="text" />
-          </div>
-          <div>
-            <span></span>
-            <input type="text" />
-          </div>
-        </div>
-        <div>
-          <span>生成</span>
+        <div id="bottomPageOrderButtonContainer">
+          <button @click="shoppingStep(1)">现在订购</button>
         </div>
       </div>
     </section>
-    <section v-if="false">
-      <div>
+    <section v-if="this.shoppingPageStep === 1">
+      <div id="orderingPageContainer">
+        <div id="orderingPageImageContainer">
+          <img src="" alt="nothing to see here" />
+        </div>
+        <div id="orderingPageDetailsInputContainer">
+          <div>
+            <span>姓名</span>
+            <input type="text" placeholder="这里输入您的姓名" />
+          </div>
+          <div>
+            <span>地址</span>
+            <input type="text" placeholder="这里输入您的地址" />
+          </div>
+          <div>
+            <span>定制化</span>
+            <input type="text" placeholder="这里输入您的定制化信息" />
+          </div>
+        </div>
+        <div id="orderingPageOrderButtonContainer">
+          <button @click="shoppingStep(2)">生成</button>
+        </div>
+      </div>
+    </section>
+    <section v-if="this.shoppingPageStep === 2" id="QRPageContainer">
+      <div id="QRPageQRContainer">
         <span>二维码</span>
-        <img src="" alt="" />
+        <qrcode
+          value="http://digitalcreak.tech"
+          id="QRPageQRContainerQrcode"
+          :options="{ width: 300 }"
+        ></qrcode>
         <svg />
       </div>
     </section>
@@ -60,11 +66,25 @@
 </template>
 
 <script>
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+
 export default {
   name: "GoodsDetails",
+  components: {
+    qrcode: VueQrcode,
+  },
+  data() {
+    return {
+      shoppingPageStep: 0,
+    };
+  },
+  methods: {
+    shoppingStep(num) {
+      this.shoppingPageStep = num;
+    },
+  },
 };
 </script>
-
 <style>
 #bottomPageGoodsDetailsContainer {
 }
@@ -114,10 +134,12 @@ export default {
   width: 500px;
   background-color: white;
 }
-#bottomPageFirstImageAndContent span {
+#bottomPageFirstImageAndContent div {
   float: left;
+  padding: 100px 300px;
   color: white;
 }
+
 #bottomPageSecondImageAndContent {
   width: 1200px;
   height: 500px;
@@ -129,8 +151,89 @@ export default {
   width: 500px;
   background-color: white;
 }
-#bottomPageSecondImageAndContent span {
-  /* float: right; */
+#bottomPageSecondImageAndContent div {
+  float: left;
+  padding: 100px 300px;
   color: white;
+}
+#bottomPageOrderButtonContainer {
+  padding-top: 100px;
+  padding-bottom: 200px;
+}
+#bottomPageOrderButtonContainer button {
+  width: 228px;
+  height: 62px;
+  font-size: 24px;
+}
+
+#orderingPageContainer {
+  height: 100%;
+  width: 100%;
+}
+
+#orderingPageImageContainer {
+  float: left;
+  padding: 300px 100px 100px 300px;
+}
+#orderingPageImageContainer img {
+  background-color: white;
+  height: 500px;
+  width: 500px;
+}
+#orderingPageDetailsInputContainer {
+  float: left;
+  padding: 300px 100px 100px 100px;
+  height: 500px;
+  width: 500px;
+}
+#orderingPageDetailsInputContainer div span {
+  font-size: 24px;
+  color: white;
+  padding-right: 30px;
+}
+#orderingPageDetailsInputContainer div {
+  padding: 50px 50px;
+}
+#orderingPageDetailsInputContainer div input {
+  height: 24px;
+  font-size: 24px;
+}
+
+#orderingPageOrderButtonContainer {
+  float: left;
+  width: 100%;
+  padding-top: 50px;
+}
+#orderingPageOrderButtonContainer button {
+  width: 228px;
+  height: 62px;
+  font-size: 24px;
+}
+#QRPageContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  padding-top: 248px;
+}
+#QRPageQRContainer {
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+  width: 600px;
+  border: 5px solid black;
+  background-color: white;
+}
+#QRPageQRContainer span {
+  font-size: 24px;
+}
+#QRPageQRContainerQrcode {
+  margin-top: 100px;
+  margin-left: 150px;
+  height: 300px;
+  width: 300px;
+}
+#QRPageQRContainer svg {
+  display: fixed;
+  border: 5px solid black;
 }
 </style>
